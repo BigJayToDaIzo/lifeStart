0f25eb5d3110d5e4b5e0734aba5c271f2e82b4df
Joseph Myers <josephlmyers@gmail.com>
2024-01-21 09:10:55 -0600

    nested data fetches into their respective components from the parent dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -37,13 +37,18 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    // Artificially delay a response for demo purposes.
+    // Don't do this in production :)
+    console.log('Fetching invoice data... ');
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
+    console.log('Invoice data fetch completed after 1.5 seconds.')
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
